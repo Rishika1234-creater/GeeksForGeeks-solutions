758117f3f288c3bb26467b3bc866d91da5803f7f
Rishika <rishikarohit1@gmail.com>
2026-06-24 19:24:06 +0530

    message

---
+++ rotatearrayby90degree.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void rotateMatrix(int[][] mat) {
+        int n = mat.length;
+        for(int i= 0;i<n;i++){
+           int left = 0;
+           int right = n-1;
+           while(left<right){
+               int temp = mat[i][left];
+               mat[i][left]=mat[i][right];
+               mat[i][right]=temp;
+               left++;
+               right--;
+           }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int temp=mat[i][j];
+                mat[i][j]=mat[j][i];
+                mat[j][i]=temp;
+            }
+        }
+    }
+}
